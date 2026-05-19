51d12c1427601af23c009bd3d3d5943ec76fc52b
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-18 21:47:42 -0300

    avance...

--- ecom-frontend/src/components/Filter.jsx
+++ ecom-frontend/src/components/Filter.jsx
@@ -27,48 +27,49 @@ const Filter = () => {
                        placeholder = "Search Products"
                        className="border border-gray-400 text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:outline-none focus:ring-2 focus:ring-[#1976d2]"
                 />
-                <FiSearch className="absolute left-3 text-slate-800" size={20} />
+                <FiSearch className="absolute left-3 text-slate-800 size={20}" />
             </div>
 
             {/* Búsqueda por Categoría seleccionada */}
             <div className="flex sm:flex-row flex-col gap-4 items-center">
-            <FormControl 
-                className="text-sl-800 border-slate-700"
-                variant="outlined"
-                size="small">
-                    <InputLabel id="category-select-label">Category</InputLabel>
-                    <Select
-                        labelId="category-select-label"
-                        value={category}
-                        onChange={handleCategoryChange}
-                        label="Category"
-                        className="min-w-[120px] text-slate-800 border-slate-700">
-                        <MenuItem value="all">All</MenuItem>
-                        {categories.map((item) => (
-                            <MenuItem key={item.categoryId} value={item.categoryName}>
-                                {item.categoryName}
-                            </MenuItem>
-                        ))}
-                    </Select>         
-            </FormControl>
-            
+                <FormControl 
+                    className="text-sl-800 border-slate-700"
+                    variant="outlined"
+                    size="small">
+                        <InputLabel id="category-select-label">Category</InputLabel>
+                        <Select
+                            labelId="category-select-label"
+                            value={category}
+                            onChange={handleCategoryChange}
+                            label="Category"
+                            className="min-w-[120px] text-slate-800 border-slate-700">
+                            <MenuItem value="all">All</MenuItem>
+                            {categories.map((item) => (
+                                <MenuItem key={item.categoryId} value={item.categoryName}>
+                                    {item.categoryName}
+                                </MenuItem>
+                            ))}
+                        </Select>         
+                </FormControl>
+                
 
-            {/* Botón de ordenamiento */}
-            <Tooltip title="Sorted by price: asc">
-                <Button variant="contained" 
-                        color="primary" 
-                        className="flex items-center gap-2 h-10">
-                    Sort By
-                    <FiArrowUp size={20}/>
-                </Button>
-            </Tooltip>
+                {/* Botón de ordenamiento */}
+                <Tooltip title="Sorted by price: asc">
+                    <Button variant="contained" 
+                            color="primary" 
+                            className="flex items-center gap-2 h-10">
+                        Sort By
+                        <FiArrowUp size={20}/>
+                    </Button>
+                </Tooltip>
 
-            {/* Bóton de limpiar filtro */}
-            <button
-                className="flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md transition duration-300 ease-in shadow-md focus:outline-hidden">
-                <FiRefreshCw className="font-semibold" size={16}/>
-                <span className="font-semibold">Clear Fitler</span>
-            </button>
+                {/* Bóton de limpiar filtro */}
+                <button
+                    onClick= ""
+                    className="flex items-center gap-2 bg-rose-900 hover:bg-rose-700 text-white px-3 py-3 h-10 rounded-md transition duration-300 ease-in shadow-md focus:outline-hidden cursor-pointer">
+                    <FiRefreshCw className="font-semibold size={16}"/>
+                    <span className="font-semibold">Clear Filter</span>
+                </button>
             </div>                
             
         </div>
