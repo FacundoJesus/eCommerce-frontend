6e7d59195064a37dbed43876c87301f4439cc125
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-23 10:20:40 -0300

    avance

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Home from './components/home/Home'
 import Products from './components/products/Products'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
 
 function App() {
 
   return (
 
     <Router>
+        <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/products' element={<Products/>}/>
