0248fada94fc27e518e79214daa4993622ca8f9f
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-26 12:08:52 -0300

    avance

--- ecom-frontend/src/components/cart/ItemContent.jsx
+++ ecom-frontend/src/components/cart/ItemContent.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { HiOutlineTrash } from "react-icons/hi";
 import SetQuantity from "./SetQuantity";
 import { useDispatch } from "react-redux";
-import { decreaseCartQuantity, increaseCartQuantity, removeFromCart } from "../../store/actions";
+import { decreaseCartQuantity, increaseCartQuantity, removeFromCart } from "../../store/actions/actions";
 import toast from "react-hot-toast";
 import { formatPrice } from "../../utils/formatPrice";
 import truncateText from "../../utils/truncateText";
@@ -43,6 +43,7 @@ const ItemContent = ({
     };
     
     return (
+        
         <div className="grid md:grid-cols-5 grid-cols-4 md:text-md text-sm gap-4   items-center  border border-slate-200  rounded-md  lg:px-4  py-4 p-2">
             <div className="md:col-span-2 justify-self-start flex  flex-col gap-2 ">
                 <div className="flex md:flex-row flex-col lg:gap-4 sm:gap-3 gap-0 items-start ">
@@ -55,24 +56,25 @@ const ItemContent = ({
                     <img 
                         src={`${import.meta.env.VITE_BACK_END_URL}/images/${image}`}
                         alt={productName}
-                        className="md:h-36 sm:h-24 h-12 w-full object-cover rounded-md"/>
+                        className="md:h-36 sm:h-24 h-12 w-full object-cover rounded-md"
+                    />
                 
 
-                <div className="flex items-start gap-5 mt-3">
-                    <button
-                        onClick={() => removeItemFromCart({
-                            image,
-                            productName,
-                            description,
-                            specialPrice,
-                            price,
-                            productId,
-                            quantity,
-                        })}
-                        className="flex items-center font-semibold space-x-2 px-4 py-1 text-xs border border-rose-600 text-rose-600 rounded-md hover:bg-red-50 transition-colors duration-200">
-                        <HiOutlineTrash size={16} className="text-rose-600"/>
-                        Remove
-                    </button>
+                    <div className="flex items-start gap-5 mt-3">
+                        <button
+                            onClick={() => removeItemFromCart({
+                                image,
+                                productName,
+                                description,
+                                specialPrice,
+                                price,
+                                productId,
+                                quantity,
+                            })}
+                            className="flex items-center font-semibold space-x-2 px-4 py-1 text-xs border border-rose-600 text-rose-600 rounded-md hover:bg-red-50 transition-colors duration-200">
+                            <HiOutlineTrash size={16} className="text-rose-600"/>
+                            Remove
+                        </button>
                     </div>
                 </div>
             </div>
