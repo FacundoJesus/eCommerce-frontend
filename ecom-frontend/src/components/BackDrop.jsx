50b194a4aaf71a52a2455f4447a8ed3d3c37b1cb
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-30 09:43:03 -0300

    ...

--- ecom-frontend/src/components/BackDrop.jsx
+++ ecom-frontend/src/components/BackDrop.jsx
@@ -1,8 +1,9 @@
-
+// Para difuminar el resto
 const BackDrop = ({ data }) => {
   return (
     <div
-        className={`z-20 transition-all duration-200 opacity-50 w-screen h-screen bg-slate-300 fixed ${data ? "top-16" : "top-0"} left-0`}
+        className={`z-20 transition-all duration-200 opacity-50 w-screen h-screen bg-slate-300 fixed ${data ? 
+        "top-16" : "top-0"} left-0`}
     >       
     </div>
   )
